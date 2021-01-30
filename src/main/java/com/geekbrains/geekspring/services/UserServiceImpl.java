b282887259ff6fe0d1101d6f90d13f12e554a23c
pro <ndg23@bk.ru>
2021-01-30 18:40:19 +0300

    add project

--- src/main/java/com/geekbrains/geekspring/services/UserServiceImpl.java
+++ src/main/java/com/geekbrains/geekspring/services/UserServiceImpl.java
@@ -4,7 +4,6 @@ import com.geekbrains.geekspring.entities.Role;
 import com.geekbrains.geekspring.entities.SystemUser;
 import com.geekbrains.geekspring.entities.User;
 import com.geekbrains.geekspring.exception.UserNotFoundException;
-import com.geekbrains.geekspring.repositories.RoleRepository;
 import com.geekbrains.geekspring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -21,88 +20,87 @@ import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
-	private UserRepository userRepository;
-	private RoleRepository roleRepository;
-	private BCryptPasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
 
-	@Autowired
-	public void setUserRepository(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-	@Autowired
-	public void setRoleRepository(RoleRepository roleRepository) {
-		this.roleRepository = roleRepository;
-	}
+    @Autowired
+    public void setPasswordEncoder(BCryptPasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
-	@Autowired
-	public void setPasswordEncoder(BCryptPasswordEncoder passwordEncoder) {
-		this.passwordEncoder = passwordEncoder;
-	}
+    @Override
+    @Transactional
+    public User findByUserName(String userName) {
+        return userRepository.findOneByUserName(userName).orElseThrow(() ->
+                new UserNotFoundException(String.format("Not found user by username: %s", userName)));
+    }
 
-	@Override
-	@Transactional
-	public User findByUserName(String userName) {
-		return userRepository.findOneByUserName(userName);
-	}
+    @Override
+    @Transactional
+    public void save(SystemUser systemUser) {
+        User user = toUser(systemUser);
+        userRepository.save(user);
+    }
 
-	@Override
-	@Transactional
-	public void save(SystemUser systemUser) {
-		User user = toUser(systemUser);
-		userRepository.save(user);
-	}
+    @Override
+    public List<SystemUser> getAll() {
+        return userRepository.findAll().stream().map(this::toSystemUser).collect(Collectors.toList());
+    }
 
-	@Override
-	public List<SystemUser> getAll() {
-		return userRepository.findAll().stream().map(this::toSystemUser).collect(Collectors.toList());
-	}
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 
-	@Override
-	public void deleteUser(Long id) {
-		userRepository.deleteById(id);
-	}
+    @Override
+    public SystemUser findById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(
+                String.format("User not found by id: %s", id)
+        ));
+        return toSystemUser(user);
+    }
 
-	@Override
-	public SystemUser findById(Long id) {
-		User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(
-				String.format("User not found by id: %s", id)
-		));
-		return toSystemUser(user);
-	}
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.findOneByUserName(userName).orElseThrow(() ->
+                new UsernameNotFoundException("Invalid username or password."));
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
+    }
 
-	@Override
-	@Transactional
-	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-		User user = userRepository.findOneByUserName(userName).orElseThrow(() ->
-				new UsernameNotFoundException("Invalid username or password."));
-		return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(),
-				mapRolesToAuthorities(user.getRoles()));
-	}
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+    }
 
-	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
-		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
-	}
+    private SystemUser toSystemUser(User user) {
+        return SystemUser.builder()
+                .id(user.getId())
+                .userName(user.getUserName())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .roles(user.getRoles())
+                .build();
+    }
 
-	private SystemUser toSystemUser(User user) {
-		return SystemUser.builder()
-				.id(user.getId())
-				.userName(user.getUserName())
-				.firstName(user.getFirstName())
-				.lastName(user.getLastName())
-				.email(user.getEmail())
-				.roles(user.getRoles())
-				.build();
-	}
-
-	private User toUser(SystemUser systemUser) {
-		return User.builder()
-				.id(systemUser.getId())
-				.userName(systemUser.getUserName())
-				.firstName(systemUser.getFirstName())
-				.lastName(systemUser.getLastName())
-				.email(systemUser.getEmail())
-				.roles(systemUser.getRoles())
-				.build();
-	}
+    private User toUser(SystemUser systemUser) {
+        return User.builder()
+                .id(systemUser.getId())
+                .userName(systemUser.getUserName())
+                .firstName(systemUser.getFirstName())
+                .lastName(systemUser.getLastName())
+                .password(
+                        (!systemUser.getPassword().isEmpty())
+                                ? passwordEncoder.encode(systemUser.getPassword())
+                                : systemUser.getPassword())
+                .email(systemUser.getEmail())
+                .roles(systemUser.getRoles())
+                .build();
+    }
 }

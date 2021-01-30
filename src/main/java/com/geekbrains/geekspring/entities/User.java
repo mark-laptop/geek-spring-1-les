d3fdb2f121a1db1db9c6c8f7e703ed3e64011efb
pro <ndg23@bk.ru>
2021-01-30 19:03:37 +0300

    add project

--- src/main/java/com/geekbrains/geekspring/entities/User.java
+++ src/main/java/com/geekbrains/geekspring/entities/User.java
@@ -1,5 +1,6 @@
 package com.geekbrains.geekspring.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import java.util.Collection;
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Table(name = "users")
 public class User {
